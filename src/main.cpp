700b426f431c95a8a258bdfbefa43ec460120940
howprice <howprice@users.noreply.github.com>
2024-04-14 17:55:55 +0100

    Add Helpers.h and increase compiler warning level

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
+#include "Helpers.h"
+
 #include <stdio.h>
+
+static void foo(int x, int y)
+{
+    HP_UNUSED(y);
+    printf("x = %d", x);
+}
 
 int main() 
 {
     printf("Hello, World!\n");
     printf("I hope this works!\n");
     printf("Let's see what happens.\n");
+
+    foo(123, 456);
     return 0;
 }
